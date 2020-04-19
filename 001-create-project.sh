11c363c17739e018a7fcfe16d3f3a314f566cb0d
System Administrator <root@iMac-od-Bopeng.local>
2020-04-18 21:40:01 -0400

    Upgrade iam codes

--- 001-create-project.sh
+++ 001-create-project.sh
@@ -1,6 +1,16 @@
 
+# Create new project ID
 echo "Please input project ID:"
 read project_id
 echo "Project ID:" $project_id
 gcloud projects create $project_id
+
+# Init GCP environment
 gcloud init
+
+# Create credential (Please replace my account and project ID to yours)
+gcloud iam service-accounts create lisa-gcp-cloud2
+gcloud projects add-iam-policy-binding lisa-gcp-cloud2 --member "serviceAccount:lisa-gcp-cloud2@lisa-gcp-cloud2.iam.gserviceaccount.com" --role "roles/owner"
+gcloud iam service-accounts keys create lisa-gcp-cloud2.json --iam-account lisa-gcp-cloud2@lisa-gcp-cloud2.iam.gserviceaccount.com
+export GOOGLE_APPLICATION_CREDENTIAL="/Users/mayerlbp/GCP-2/3/terraform-google-gke/lisa-gcp-cloud2.json"
+
