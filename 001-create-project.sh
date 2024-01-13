21522f3810cd325211ed590fa441f50ef740e176
mayerll <mayerlbp@hotmail.com>
2024-01-13 16:09:35 -0500

    Update 001-create-project.sh

--- 001-create-project.sh
+++ 001-create-project.sh
@@ -14,7 +14,7 @@ gcloud projects add-iam-policy-binding datapipeline --member "serviceAccount:dat
 gcloud iam service-accounts keys create datapipeline.json --iam-account datapipeline@datapipeline.iam.gserviceaccount.com
 export GOOGLE_APPLICATION_CREDENTIALS="/Users/mayerlbp/datapipeline-gcp/GCP/datapipeline.json"
 
-# Run the following commands to set up GKE (Please input region, project ID and cluster name)
+# Run the following commands to test whether we set up GKE successfully (Please input region, project ID and cluster name)
 terraform init
 terraform plan
 terraform apply
@@ -22,7 +22,7 @@ terraform apply
 # Set up kubernetes proxy
 kubectl proxy &
 
-#Get endpoints information
+#Get endpoints information for verifying the components set up by Terraform.
 curl http://localhost:8001/api/v1/namespaces/default/endpoints
 
 # Get nginx service external IP
